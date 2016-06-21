a5c31683eeb810b8fa1b550d8541c4f086f0448c
Ashkan Mehrkar <ashkan.mm97@gmail.com>
2016-06-21 18:28:21 +0430

    Hello class was added.

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 /**
  * Created by ashkanmehrkar on 6/21/16.
  */
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }

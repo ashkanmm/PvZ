48e69a35438fee7a8bac9bfd0e4028067ee9e4eb
ashkanmm <ashkan.mm97@gmail.com>
2016-06-24 14:34:52 +0430

    Images were updated

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -11,7 +11,7 @@ public class MainWindow extends JFrame {
     int state = 0;
     int stateState = 0;
     public MainWindow() {
-        super("گیاهان و زامبی ها");
+        super("گیاهان در برابر زامبی ها");
         addMouseMotionListener(new MouseMotionListener() {
             @Override
             public void mouseDragged(MouseEvent e) {
